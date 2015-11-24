9955c776f777982c1b2793fb3234793b8df436b1
Milton Moura <miltonmoura@gmail.com>
2015-11-24 17:11:45 -0100

    fixing double paste on FromRiverOne

--- Lesson 2 - FromRiverOne.m
+++ Lesson 2 - FromRiverOne.m
@@ -25,31 +25,4 @@ int solution(int X, NSMutableArray *A) {
     }
 
     return -1;
-}int solution(int X, NSMutableArray *A) {
-    NSMutableArray * C = [@[] mutableCopy];
-
-    for (int i = 0; i <= X; i++) {
-        [C addObject:@0];
-    }
-
-    int minJump = 1;
-    for (int i = 0; i < [A count]; i ++) {
-        int step = [A[i] intValue];
-        C[step] = @1;
-
-        bool canJump = true;
-        for (int j = minJump; j <= X; j++) {
-            if ([C[j] isEqual:@0]) {
-                minJump = j;
-                canJump = false;
-                break;
-            }
-        }
-
-        if (canJump) {
-            return i;
-        }
-    }
-
-    return -1;
 }
